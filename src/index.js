66b1dfdcb09293fdfd51ae22e1e8f558e048538e
nick <crismas90@gmail.com>
2017-04-24 13:14:35 +0300

    Indents in the code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
 let textArea = $(".message-field");
 let output = $(".message-formatted");
-textArea.on("input", function() {
-let value = textArea.val();
-function textFormatting(){
-return value.toLowerCase().trim().replace(/\s+/g, ' ');
-}
-output.text(textFormatting(value));
+textArea.on("input", function () {
+    let value = textArea.val();
+    function textFormatting() {
+        return value.toLowerCase().trim().replace(/\s+/g, ' ');
+    }
+    output.text(textFormatting(value));
 });
