78c657df9e4d59b9730dcc4233765c3e83be0a13
nick <crismas90@gmail.com>
2017-04-24 13:12:07 +0300

    text formatting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 'use strict';
-
-console.log($)
+let textArea = $(".message-field");
+let output = $(".message-formatted");
+textArea.on("input", function() {
+let value = textArea.val();
+function textFormatting(){
+return value.toLowerCase().trim().replace(/\s+/g, ' ');
+}
+output.text(textFormatting(value));
+});
